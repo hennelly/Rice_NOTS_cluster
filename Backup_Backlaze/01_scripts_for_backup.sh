42c7d4004ab2189ae7fb32f606bb66d3025e793a
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-12-30 11:31:10 -0600

    Create 01_scripts_for_backup.sh

---
+++ Backup_Backlaze/01_scripts_for_backup.sh
@@ -0,0 +1,14 @@
+# Move files from remote server to blackblaze 
+
+HennellyLabBackup key: 
+
+KeyID: 35dc2e1b4a6a
+
+Application Key ID: K005MsaYJIrEzjEr5G6QasUObBbXIJE
+
+Scripts for moving files over to backblaze 
+
+rclone sync /home/crq857/projects/Redwolf HennellyLabBackup:HennellyLabBackup/Projects_CPH/Redwolf
+
+Username: laurenmaehennelly@gmail.com
+Password: RiceUniversity1
