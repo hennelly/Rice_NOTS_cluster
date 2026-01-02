676269a30a3a5b181e284d522997dcaf9381b305
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-01-02 14:59:24 -0600

    Update files_on_NOTS.sh

--- Files on NOTS/files_on_NOTS.sh
+++ Files on NOTS/files_on_NOTS.sh
@@ -8,3 +8,10 @@ curl
 https://sid.erda.dk/share_redirect/Hvjfs9cMxP/reference_panel_vcf/1697g_WildSled.SNP.INDEL.chrAll.newID.vcf.gz
 --output g_WildSled.SNP.INDEL.chrAll.newID.vcf.gz
 
+
+
+rclone sync /home/crq857/projects/Redwolf HennellyLabBackup:HennellyLabBackup/Projects_CPH/Redwolf
+
+
+
+
