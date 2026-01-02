e6506afb2c84890905e45d2962fae18625219e1d
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-01-02 15:06:33 -0600

    Update Upload_desktop_to_NOTS.sh

--- Upload_desktop_to_NOTS.sh
+++ Upload_desktop_to_NOTS.sh
@@ -1,3 +1,3 @@
 
 
-rsync -avz -e "ssh -p 2022" --progress ~/Desktop/keepwolves_Oct_22_2025.txt hennelly@farm.cse.ucdavis.edu:/home/hennelly/Pakistan_Graywolf_Genomics/Mitogenome
+rsync -avz --progress ~/Desktop/Graywolf_Israel_MW172_2.fq.gz lh106@nots.rice.edu:/home/lh106/PreviousProjects
