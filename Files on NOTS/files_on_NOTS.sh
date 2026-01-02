b452bece30b5deb6efac1f871b5dc7ffe1ff904d
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-01-02 14:57:00 -0600

    Update files_on_NOTS.sh

--- Files on NOTS/files_on_NOTS.sh
+++ Files on NOTS/files_on_NOTS.sh
@@ -3,3 +3,8 @@ https://sid.erda.dk/share_redirect/Hvjfs9cMxP/reference_panel_vcf/1697g_WildSled
 --output g_WildSled.SNP.INDEL.chrAll.newID.vcf.gz
 
 
+
+curl
+https://sid.erda.dk/share_redirect/Hvjfs9cMxP/reference_panel_vcf/1697g_WildSled.SNP.INDEL.chrAll.newID.vcf.gz
+--output g_WildSled.SNP.INDEL.chrAll.newID.vcf.gz
+
