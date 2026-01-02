0fd35511922ebf75c46d98f2c0c46c078407d803
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-01-02 15:35:08 -0600

    Update Upload_desktop_to_NOTS.sh

--- Upload_desktop_to_NOTS.sh
+++ Upload_desktop_to_NOTS.sh
@@ -1,3 +1,3 @@
 
 
-rsync -avz --progress ~/Desktop/Graywolf_Israel_MW172_2.fq.gz lh106@nots.rice.edu:/home/lh106/PreviousProjects
+rsync -avz --progress ~/Desktop/Graywolf_Israel_MW174_1.fq.gz lh106@nots.rice.edu:/home/lh106/PreviousProjects
