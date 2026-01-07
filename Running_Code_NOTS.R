882ce002b4177f94d9dd246bcfd71e364a354e95
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-01-06 18:14:55 -0600

    Update Running_Code_NOTS.R

--- Running_Code_NOTS.R
+++ Running_Code_NOTS.R
@@ -8,6 +8,8 @@
 #SBATCH --output=/home/lh106/Rice/logfiles/out3.out
 #SBATCH --error=/home/lh106/Rice/logfiles/out3.err
 
+mv Africanwolf_Algeria_MJ109* /storage/hpc/work/lh106/bin/ncftp-3.3.0/bin 
+
 cat /scratch/lh106/rawfiles/V300066577_L2_DKWGS201203039-531_1.fq.gz /scratch/lh106/rawfiles/V300066577_L2_DKWGS201203039-532_1.fq.gz /scratch/lh106/rawfiles/V300074477_L1_DKWGS201203039-531_1.fq.gz /scratch/lh106/rawfiles/V300074477_L1_DKWGS201203039-532_1.fq.gz /scratch/lh106/rawfiles/V300074519_L4_DKWGS201203039-531_1.fq.gz /scratch/lh106/rawfiles/V300074519_L4_DKWGS201203039-532_1.fq.gz > /scratch/lh106/rawfiles/Africanwolf_Algeria_MJ109_1.fq.gz
 cat /scratch/lh106/rawfiles/V300066577_L2_DKWGS201203039-531_2.fq.gz /scratch/lh106/rawfiles/V300066577_L2_DKWGS201203039-532_2.fq.gz /scratch/lh106/rawfiles/V300074477_L1_DKWGS201203039-531_2.fq.gz /scratch/lh106/rawfiles/V300074477_L1_DKWGS201203039-532_2.fq.gz /scratch/lh106/rawfiles/V300074519_L4_DKWGS201203039-531_2.fq.gz /scratch/lh106/rawfiles/V300074519_L4_DKWGS201203039-532_2.fq.gz > /scratch/lh106/rawfiles/Africanwolf_Algeria_MJ109_2.fq.gz
 
