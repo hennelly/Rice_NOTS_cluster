5ad52221a7baf05ab2f7ffca6eea345a07702ebc
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-01-06 18:04:49 -0600

    Create Pipeline to move files to NCBI SRA from NOTS.R

---
+++ Pipeline to move files to NCBI SRA from NOTS.R
@@ -0,0 +1,13 @@
+
+
+/scratch/lh106/rawfiles
+
+Africanwolf_Algeria_MJ099_1.fq.gz
+Africanwolf_Algeria_MJ099_2.fq.gz
+
+Africanwolf_Algeria_MJ109_1.fq.gz
+Africanwolf_Algeria_MJ109_2.fq.gz
+
+
+/storage/hpc/work/lh106/bin/ncftp-3.3.0/bin
+
